190d2da5016afaa5b158ac6d13c8fd6acc3d8a21
andres <andres10penalva@gmail.com>
2023-03-29 18:07:14 +0200

    segundo test realizado

--- TDD Practica Coche/tests/testCoche.java
+++ TDD Practica Coche/tests/testCoche.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class testCoche {
     @Test
-    public void test_crear_coche(){
+    public void test_crear_coche_vel_es_cero(){
         Coche nuevoCoche = new Coche();
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
